3c9be3028ffda52375fba99d7f7a769bff066515
rupi <varinderjit17@gmail.com>
2024-09-20 09:07:27 +1200

    Input number validation

--- src/main/java/com/rupicodes/mvc/model/Customer.java
+++ src/main/java/com/rupicodes/mvc/model/Customer.java
@@ -1,5 +1,7 @@
 package com.rupicodes.mvc.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -11,6 +13,18 @@ public class Customer {
     @NotNull(message = "Last Name is required")
     @Size(min=3, message = "Atleast 3 characters required")
     private String lastName;
+
+    @Min(value=0, message="Value must be greater than or equal to zero")
+    @Max(value=10, message="Value must be less than or equal to 10")
+    private int freePass;
+
+    public int getFreePass() {
+        return freePass;
+    }
+
+    public void setFreePass(int freePass) {
+        this.freePass = freePass;
+    }
 
     public Customer() {}
 
